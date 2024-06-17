30560890b957aa5487df3bcf6d7c8647bb2154ad
Diego Luna <dpl@cesar.school>
2024-06-17 18:00:59 -0300

    feat: add meals schema

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -12,5 +12,15 @@ declare module "knex/types/tables" {
       created_at: string;
       updated_at: string;
     };
+    meals: {
+      id: string;
+      user_id: string;
+      name: string;
+      description: string;
+      is_on_diet: boolean;
+      date: string;
+      created_at: string;
+      updated_at: string;
+    };
   }
 }
